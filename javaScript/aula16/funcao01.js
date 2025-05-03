7d976dd5930fe4cf3f2bafe64fbc7ef0ed1f4bb6
Jhon Jackson S S <radicalug@gmail.com>
2025-05-03 13:20:40 -0300

    31

---
+++ javaScript/aula16/funcao01.js
@@ -0,0 +1,19 @@
+/*function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+       return 'Ímpar!'
+    }
+}
+
+console.log(parimpar(10))
+
+let res = parimpar(11)
+console.log(res)*/
+
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+
+console.log(soma(13, 7))
+console.log(soma(15))
