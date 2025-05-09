93e5f62186cec76be32819308fccef2168689447
Jhon Jackson S S <radicalug@gmail.com>
2025-05-09 13:03:24 -0300

    34

--- javaScript/aula16ex/ex16script.js
+++ javaScript/aula16ex/ex16script.js
@@ -1,12 +1,34 @@
-function adc() {
-    let adc = document.getElementById('txtn')
-    let adcnum = document.getElementById('seladcnum')
-    let num = Number(adc.value)
-    if (adc.value.length == 0) {
-        alert("por favor , digite os dados!")
-    } else if (num < 1 || num > 100) {
-        alert("por favor, digite os dados entre 1 e 100!")
-    } else {
+let num = document.getElementById('txtn')
+let lista = document.getElementById('seladcnum')
+let res = document.querySelector('div#res')
+let valores = []
 
+function IsNumero(n) {
+    if(Number(n) >= 1 && Number(n) <=100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function msg() {
+    let MSG = "Valor invalido ou já encontrado na lista!"
+    return MSG
+    
+}
+
+function adc() {
+    if(IsNumero(num.value) && !inLista(num.value, valores)) {
+
+    } else {
+        alert(msg())
     }
 }
